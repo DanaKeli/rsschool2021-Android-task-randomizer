48c3f167938566cbb6fdf579fedcb2c735b60a07
DanaKeli <79215094+DanaKeli@users.noreply.github.com>
2021-05-30 18:54:52 +0300

    feat: complete Second Fragment

--- app/src/main/java/com/rsschool/android2021/SecondFragment.kt
+++ app/src/main/java/com/rsschool/android2021/SecondFragment.kt
@@ -1,5 +1,6 @@
 package com.rsschool.android2021
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,11 +8,26 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import java.util.concurrent.ThreadLocalRandom
+import kotlin.random.Random as Random1
 
 class SecondFragment : Fragment() {
 
     private var backButton: Button? = null
     private var result: TextView? = null
+    private var listener: OnBackButtonPressedListener? = null
+    private var randomNum = 0
+
+    interface OnBackButtonPressedListener {
+        fun onBackButtonPressed(previousNumber: Int)
+        fun onSystemBackButtonPressed(previousNumber: Int)
+       // fun onBackPressed()
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        listener = context as OnBackButtonPressedListener
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,16 +45,20 @@ class SecondFragment : Fragment() {
         val min = arguments?.getInt(MIN_VALUE_KEY) ?: 0
         val max = arguments?.getInt(MAX_VALUE_KEY) ?: 0
 
-        result?.text = generate(min, max).toString()
+        randomNum = generate(min, max)
+        listener?.onSystemBackButtonPressed(randomNum)
+
+        result?.text = randomNum.toString()
 
         backButton?.setOnClickListener {
             // TODO: implement back
+            listener?.onBackButtonPressed(randomNum)
         }
     }
 
     private fun generate(min: Int, max: Int): Int {
         // TODO: generate random number
-        return 0
+        return ((Math.random() * (max - min + 1) + min).toInt())
     }
 
     companion object {
@@ -47,12 +67,12 @@ class SecondFragment : Fragment() {
         fun newInstance(min: Int, max: Int): SecondFragment {
             val fragment = SecondFragment()
             val args = Bundle()
-
             // TODO: implement adding arguments
-
+            args.putInt(MIN_VALUE_KEY, min)
+            args.putInt(MAX_VALUE_KEY, max)
+            fragment.arguments = args
             return fragment
         }
-
         private const val MIN_VALUE_KEY = "MIN_VALUE"
         private const val MAX_VALUE_KEY = "MAX_VALUE"
     }
